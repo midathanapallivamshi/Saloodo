b6d7bd9522b8b5ffb0a9c39152680190f7098220
midathanapallivamshi <midathanapallivamshi@users.noreply.github.com>
2019-09-18 23:50:25 +0530

    Updated Comment lines to understand the script

--- Question one challange( return reverse of string)/reversestring.py
+++ Question one challange( return reverse of string)/reversestring.py
@@ -1,23 +1,38 @@
+#!/usr/bin/env python3
+#
+#
+# 
 import random
-import json
+# Json module import only if we are reading json input reading and currently we are not reading json input and hence commenting out
+# import json
 
 def reverse_string(str_input):
-    return str_input[1:]
+    """
+    This method is used to reverse a string
+    """
+    reverse_string = str_input[1:]
+    return reverse_string
 
 def generate_random_number():
-    num = random.randint(1,10000)
-    return num
+    """
+    This method is used to generate random number between 1 and 10000
+    """
+    random_number = random.randint(1,10000)
+    return random_number
 
-
+# defining in dict format and later required to replace with json input readable code. 
 input_json = {"message": "abcdef"}
 
+# empty dict to store output
 output = {}
 
-
+# Get reverse string 
 get_reverse_str = reverse_string(input_json["message"])
 output["message"] = get_reverse_str
 
+# Get a random number
 random_number = generate_random_number()
-
 output["random"] = random_number
+
+# Print output to console
 print(json.dumps(output))
